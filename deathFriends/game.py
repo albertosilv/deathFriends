a2975d2e928d9f903656e19ef2f4bb2506fac884
albertosilv <jose.alberto.silva@ccc.ufcg.edu.br>
2020-11-16 13:27:12 -0300

    imports de fontes e criaçao do menu inicial

--- deathFriends/game.py
+++ deathFriends/game.py
@@ -1,5 +1,5 @@
 import pygame as pg
-import sys
+from os import path
 from settings import *
 
 class Game:
@@ -12,7 +12,16 @@ class Game:
         self.load_data()
 
     def load_data(self):
-        pass
+        game_folder = path.dirname(__file__)
+
+        image_folder = path.join(game_folder, 'sprints')
+        text_folder = path.join(game_folder, 'fonts')
+
+        self.backgroudMenu_img = pg.image.load(
+            path.join(image_folder, BACKGROUNDMENU))
+        self.titleFont = pg.font.Font(path.join(text_folder, TILEFONT), 45)
+        self.fontMenu = pg.font.Font(path.join(text_folder, MENUFONT), 35)
+
 
     def new(self):
        pass
@@ -43,7 +52,36 @@ class Game:
             if event.type == pg.QUIT:
                 self.quit()
     def show_start_screen(self):
-        pass
+         while(True):
+            for e in pg.event.get():
+                if(e.type == pg.QUIT):
+                    self.quit()
+            self.screen.blit(self.backgroudMenu_img, (0, 0))
+
+            # Titulo do jogo
+            self.textObjetc('death friends', self.titleFont, 5)
+
+            # Botoes
+            self.textObjetc('Iniciar jogo', self.fontMenu, 2.5)
+            self.textObjetc('Configurações', self.fontMenu, 2)
+            self.textObjetc('Sair', self.fontMenu, 1.7)
+
+            mousePosition = pg.mouse.get_pos()
+            click = pg.mouse.get_pressed()
+            if(400 < mousePosition[0] < 565 and 245 < mousePosition[1] < 275) and click[0] == 1:
+                return 1
+            if(370 < mousePosition[0] < 585 and 305 < mousePosition[1] < 330 and click[0] == 1):
+                self.quit()
+            if(450 < mousePosition[0] < 510 and 360 < mousePosition[1] < 385 and click[0] == 1):
+                self.quit()
+
+            self.clock.tick(FPS)
+            pg.display.flip()
 
     def show_go_screen(self):
         pass
+    def textObjetc(self, text, font, location):
+        textSurf = font.render(text, True, WHITE)
+        textReact = textSurf.get_rect()
+        textReact.center = (int(WIDTH/2), int(HEIGHT/location))
+        self.screen.blit(textSurf, textReact)
