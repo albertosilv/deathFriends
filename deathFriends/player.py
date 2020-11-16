a2585751cb5be5aa6f489e6264fa0cc82ad5e84b
albertosilv <jose.alberto.silva@ccc.ufcg.edu.br>
2020-11-16 13:35:39 -0300

    movimentaçao do player

--- deathFriends/player.py
+++ deathFriends/player.py
@@ -11,3 +11,18 @@ class Player(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.vel=vect(0,0)
         self.pos=vect(x,y)*TILESIZE
+    def get_keys(self):
+            self.vel = vect(0,0)
+            keys = pg.key.get_pressed()
+            if keys[pg.K_LEFT] or keys[pg.K_a]:
+                self.vel.x = -PLAYER_SPEED
+            if keys[pg.K_RIGHT] or keys[pg.K_d]:
+                self.vel.x = PLAYER_SPEED
+            if keys[pg.K_UP] or keys[pg.K_w]:
+                self.vel.y = -PLAYER_SPEED
+            if keys[pg.K_DOWN] or keys[pg.K_s]:
+                self.vel.y = PLAYER_SPEED
+            if self.vel.x != 0 and self.vel.y != 0:
+                self.vel*= 0.7071
+    def update(self):
+        self.get_keys()
